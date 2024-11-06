5ba3dd5ec3d53cee67d3c81b43d6e9d16c193201
Eventlistener <chanfcsg334@gmail.com>
2024-11-06 22:08:37 +0700

    project

---
+++ src/main/java/com/chantha2121/restfull_demo/RestfullDemoApplication.java
@@ -0,0 +1,14 @@
+package com.chantha2121.restfull_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(
+				RestfullDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chantha2121/restfull_demo/RestfullDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chantha2121.restfull_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
